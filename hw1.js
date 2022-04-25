ee0bda5a8eaf205d1dbb2422f3ac5b1024989436
webbe32 <webb.edwin@gmail.com>
2022-04-25 13:23:18 -0400

    hw1 1-13

---
+++ hw1.js
@@ -0,0 +1,25 @@
+let myVariable = "walking"; 
+myVariable = "running"; 
+console.log(myVariable)
+
+const myVar2 = 10; 
+if (true) {
+    var x = "variableX"
+}
+console.log(x);
+
+if(true){
+    let y = "variableY"
+}
+// console.log(y); 
+
+// myVar2 = 25; 
+// console.log(myVar2); 
+
+const myEmptyArr = []; 
+myEmptyArr.push(5, 10, 15);
+console.log(myEmptyArr); 
+
+const myEmptyObj = {};
+myEmptyObj.name = "Edwin";
+myEmptyObj.age = 32; 
